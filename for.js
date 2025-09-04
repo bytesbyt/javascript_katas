14d260968edfe24ce4f3e64818fa52af248b2839
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-09-04 19:59:45 +0100

    complete ex 4(for)

--- for.js
+++ for.js
@@ -5,3 +5,31 @@ for (let i = 1; i < 101; i++) {
     sum += i;
 }
 console.log(sum);
+
+// 2
+for ( let i = 1; i < 101; i+=2) {
+    console.log(i)
+}
+
+// 3
+
+for(let i = 1; i <= 50; i++) {
+    let matches = i.toString().match(/[369]/g); //Regex: [369]는 3, 6, 9를 찾는 패턴, g(Global)는 모든 매칭을 찾는 플래그
+    console.log(matches ? '짝'.repeat(matches.length) : i); 
+}
+
+//  4
+let input = 12;
+let isPrimeNum = true;
+
+if (input === 1) {
+    isPrimeNum = false;
+}
+
+for (let i = 2; i < input; i++) {
+    if (input % i === 0) {
+        isPrimeNum = false;
+        break;
+    }
+}
+console.log(isPrimeNum);
